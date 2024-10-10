371f4b6d18294b209cc09f4566cbf686111447b4
Harsh kasana <bt22ece106@gmail.com>
2024-10-11 01:05:04 +0530

    Added the projects page

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ import React from 'react'
 
 export default function Portfolio() {
   return (
-    <div className="min-h-screen bg-gradient-to-r from-gray-800 to-black text-gray-100">
+    <div className="min-h-screen bg-gradient-to-r from-gray-800 to-black  text-gray-100">
       <Header />
       <main className="pt-16">
         <Hero />
